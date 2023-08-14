0bf3df3143a88df0304a2caec1f6ae55f764d7df
Brian Winkers <bwinkers@gmail.com>
2023-08-14 15:38:23 -0700

    Placeholder useVue.org website

---
+++ usevue.org/src/layouts/MainLayout.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <!-- <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        /> -->
+
+        <q-toolbar-title>
+          useVue
+        </q-toolbar-title>
+
+
+      </q-toolbar>
+    </q-header>
+
+    <!-- <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer> -->
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar >
+        <q-toolbar-title>
+
+          <div class="text-center justify-center">An <a href="https://www.ulri.com" class="navbar-text-link">Ultri</a> project</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.u-navbar-light {
+  background-color: white;
+  color: $navbar-txt-lt;
+}
+.u-navbar-dark {
+  background-color: $dark-page;
+  color: $navbar-txt-dk;
+}
+</style>
---
+++ usevue.org/src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="row text-h1 full-width text-center justify-center">Coming soon...</div>
+    <div class="row text-h2">Vue advocacy and training</div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
